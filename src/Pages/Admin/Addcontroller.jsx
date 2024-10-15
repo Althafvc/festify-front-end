196cb869fb99538d5f249d6ef20b79c074391976
Althaf vc <althafvellanchola46@gmail.com>
2024-10-15 23:16:21 +0530

    made some changes in addcontroller page in adminside

--- src/Pages/Admin/Addcontroller.jsx
+++ src/Pages/Admin/Addcontroller.jsx
@@ -4,13 +4,13 @@ import Adminsidebar from '../../Components/Admin/Adminsidebar'
 
 function Addcontroller() {
   return (
-    <>
+    <> 
         <Adminsidebar/>
 
-    <div className=' xxs:w-full xxs:h-full xxs:flex xxs:flex-col xxs:gap-5 xxs:justify-center xxs:items-center xxs:py-5  border border-red-700'>
+    <div className=' xxs:w-full xxs:h-full xxs:flex xxs:flex-col xxs:gap-5 xxs:justify-center xxs:items-center xxs:py-5'>
 
         <div className="head-area ">
-          <h1 className='font-poppins xxs:text-xl font-semibold text-[#38896b]'>Add a controller here</h1>
+          <h1 className='font-poppins xxs:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-semibold text-[#38896b]'>Add a controller here</h1>
         </div>
         <div className="slate w-[85%] h-auto bg-[#89deba] rounded-2xl xxs:py-8 xxs:flex xxs:flex-col xxs:items-center">
 
